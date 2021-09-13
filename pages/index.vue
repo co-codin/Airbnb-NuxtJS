371726f2cebf41f0453f593835b2a51ae04e6d22
ElijahCoding <cuiyeqing960904@gmail.com>
2021-09-13 19:33:34 +0300

    seo

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,15 @@
 
 <script>
 export default {
-
+  head() {
+    return {
+      title: "homepage",
+      meta: [{
+        name: 'description',
+        content: 'home page',
+        hid: 'description',
+      }]
+    }
+  }
 }
 </script>
